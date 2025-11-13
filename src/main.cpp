720c66d9df943a4bda8dfbdff7ca27eb88dacfe7
SoftDev-Candy <swastiktoprani@gmail.com>
2025-11-13 12:37:56 +0100

    Learning openGL and graphicsPipeline

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,57 @@
 #include<iostream>
+#include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
-int main(void) {
-    GLFWwindow* window;
+int main(void)
+{
 
-//Initialize the Library
-    if (!glfwInit()) {
-        std::cout << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
+    //Initialise GLfW
+if (!glfwInit())
+{
+    std::cout << "Failed to initialize GLFW" << std::endl;
+    return -1;
+}
 
-    //create a windowed mode window and with openGL //
-    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
-    if (!window)
+    //give hints on what version you are using
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE , GLFW_OPENGL_CORE_PROFILE);
+
+    //Create window here now if not created terminate that mfer
+    GLFWwindow* window = glfwCreateWindow(800, 800, "B_WEngine", nullptr, nullptr);
+    if (window == NULL)
     {
+        std::cout<<"Failed to create GLFW Window"<<std::endl;
         glfwTerminate();
         return -1;
     }
 
-    glfwMakeContextCurrent(window);
-    std::cout << window << std::endl;
+    //making window the current context //
+glfwMakeContextCurrent(window);
 
-    while (!glfwWindowShouldClose(window))
+//Initialize Glad//
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-
-        glfwPollEvents();
-
+        std::cerr<<"Failed to initialize GLAD"<<std::endl;
+        return -1;
     }
 
-    glfwTerminate();
-    return 0;
+    glViewport(0,0,800,800); //This is the viewport or the drawable size area
+
+    //Its while loop to keep that window open//
+while (!glfwWindowShouldClose(window))
+{
+    glfwPollEvents();
+    glClearColor(.12f,0.13f,0.17f,1.0f); //The colour we want to give to the screen
+
+    glClear(GL_COLOR_BUFFER_BIT);//Clearing the buffer to get the colour out.
+    glfwSwapBuffers(window);//Swapping buffers to make colour come in front on the viewport.
+
 }
+
+    glfwDestroyWindow(window);
+glfwTerminate();
+}
+
+
+
