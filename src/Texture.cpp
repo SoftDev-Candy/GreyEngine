53407420447cc8fe9c814e4935c0139dd42fdb33
SoftDev-Candy <swastiktoprani@gmail.com>
2025-12-08 20:56:40 +0100

    Updated the  Texture class so that we can add texture to da cube

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -3,11 +3,13 @@
 //
 
 #include "Texture.hpp"
-
 #include <iostream>
-
-#include "stb_image.h"
+//Defining that macro//
 #define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "glad/glad.h"
+#include "glfw/glfw3.h"
+
 //RAII is very important although could've named it literally anything else ...resource allocation is initailzation...WDUM<???//
 Texture::Texture(const char *path)
 {
@@ -16,11 +18,52 @@ Texture::Texture(const char *path)
     int channels;
 
   unsigned char* storedata =  stbi_load(path , &width , &height, &channels,0);
+    GLenum internalFormat = 0;
+    GLenum dataFormat = 0;
+if (channels == 3)
+{
+    internalFormat =GL_RGB;
+        dataFormat = GL_RGB;
+}
+else if (channels == 4)
+{
+    internalFormat =GL_RGBA;
+    dataFormat = GL_RGBA;
+}
+    else
+    {
+        std::cerr<<"Error in texture handling for colour formats"<<std::endl;
+    }
 
         if(storedata == NULL)
         {
             std::cerr<<"The data for texture was NULL "<<std::endl;
         }
+
+    //First we generate the texture
+    glGenTextures(1,&id);
+    glBindTexture(GL_TEXTURE_2D, id);
+
+glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+
+
+    glTexImage2D(
+     GL_TEXTURE_2D,     // always GL_TEXTURE_2D for us
+     0,                 // mipmap level (always 0 for now) don't have any basically
+     internalFormat,    // how GPU should store the texture (RGB? RGBA?) imp shit
+     width, height,     // image size
+     0,                 // Border = always 0 for now
+     dataFormat,        // how OUR image is laid out (RGB or RGBA) thats how we will see it basically
+     GL_UNSIGNED_BYTE,  // GL_UNSIGNED_BYTE (stbi loads 8-bit)
+     storedata          // the pixel pointer (storedata)
+ );
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(storedata);
+
 }
 
 Texture::~Texture()
